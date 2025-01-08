ad3f573fe4deaddc443dfeca49ca4c9e80cfb5b3
pranjalr004 <pranjalr376@gmail.com>
2025-01-08 18:51:16 +0530

    Carousel Added

---
+++ client/src/components/home/Banner.jsx
@@ -0,0 +1,46 @@
+import Carousel from "react-multi-carousel";
+import { bannerData } from "../../constants/data";
+import "react-multi-carousel/lib/styles.css";
+
+import { styled } from "@mui/material";
+
+const Image = styled('img')`
+    width:100%;
+    height:280;`
+
+const responsive = {
+    desktop: {
+        breakpoint: { max: 3000, min: 1024 },
+        items: 1
+    },
+    tablet: {
+        breakpoint: { max: 1024, min: 464 },
+        items: 1
+    },
+    mobile: {
+        breakpoint: { max: 464, min: 0 },
+        items: 1
+    }
+};
+
+function Banner() {
+    return (
+        <Carousel responsive={responsive}               dotListClass="custom-dot-list-style"
+            itemClass="carousel-item-padding-40-px" containerClass="carousel-container"
+            swipeable={false}
+            draggable={false}
+            infinite={true}
+            autoPlay={true}
+            autoPlaySpeed={4000}
+            keyBoardControl={true}
+            slidesToSlide={1} >
+            {
+                bannerData.map(data => (
+                    <Image src={data.url} alt="banner" />
+                ))
+            }
+        </Carousel>
+    )
+}
+
+export default Banner
--- client/src/components/home/Home.jsx
+++ client/src/components/home/Home.jsx
@@ -1,8 +1,20 @@
+import { Box,styled } from "@mui/material"
+import Banner from "./Banner"
 import Navbar from "./Navbar"
+
+const Component=styled(Box)`
+    padding:10px;
+    background:#F2F2F2;`
 
 const Home=()=>{
     return (
-        <Navbar/>
+        <>
+            <Navbar/>
+            <Component>
+            <Banner/>
+            </Component>
+        </>
+        
     )
 }
 
