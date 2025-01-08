df02c279f74ccfaeeafaa9c3ae9c7a6d56b6dc38
pranjalr004 <pranjalr376@gmail.com>
2025-01-08 21:40:51 +0530

    Data Added using MongoDB

--- server/database/db.js
+++ server/database/db.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 export const Connection=async(username,password)=>{
     const URL=`mongodb://${username}:${password}@ecommerce-web-shard-00-00.7l9xu.mongodb.net:27017,ecommerce-web-shard-00-01.7l9xu.mongodb.net:27017,ecommerce-web-shard-00-02.7l9xu.mongodb.net:27017/project1?ssl=true&replicaSet=atlas-b0mjbe-shard-0&authSource=admin&retryWrites=true&w=majority&appName=ecommerce-web`
     try{
-       await mongoose.connect(URL,{useUnifiedTopology:true,useNewUrlParser:true})
+       await mongoose.connect(URL)
        console.log("Database Connected Successfully")
     } 
     catch(error){
--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ import express from "express"
 import dotenv from "dotenv"
 
 import Connection from "./database/db.js";
+import DefaultData from "./default.js";
 
 const app=express()
 dotenv.config()
@@ -12,3 +13,5 @@ const PASSWORD=process.env.DB_PASSWORD;
 Connection(USERNAME,PASSWORD)
 
 app.listen(PORT,()=>console.log(`Server is running successfully on PORT ${PORT}`))
+
+DefaultData()
