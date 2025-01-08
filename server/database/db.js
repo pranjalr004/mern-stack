1b9c770248d3a4ab7e22ef662f7c716d4182876e
pranjalr004 <pranjalr376@gmail.com>
2025-01-08 21:06:02 +0530

    Database created & connected

---
+++ server/database/db.js
@@ -0,0 +1,14 @@
+import mongoose from "mongoose";
+
+export const Connection=async(username,password)=>{
+    const URL=`mongodb://${username}:${password}@ecommerce-web-shard-00-00.7l9xu.mongodb.net:27017,ecommerce-web-shard-00-01.7l9xu.mongodb.net:27017,ecommerce-web-shard-00-02.7l9xu.mongodb.net:27017/project1?ssl=true&replicaSet=atlas-b0mjbe-shard-0&authSource=admin&retryWrites=true&w=majority&appName=ecommerce-web`
+    try{
+       await mongoose.connect(URL,{useUnifiedTopology:true,useNewUrlParser:true})
+       console.log("Database Connected Successfully")
+    } 
+    catch(error){
+        console.log("Error while Connecting with the database",error.message);
+    }
+}
+
+export default Connection
