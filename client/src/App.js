94929d615e6c5877f517481ef9b63dfce9b341bf
pranjalr004 <pranjalr376@gmail.com>
2025-01-08 18:23:55 +0530

    Navbar for images added

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,16 @@
-import './App.css';
+import { Box } from '@mui/material';
 import Header from './components/Header/header';
+import Home from './components/home/Home';
+
+
 
 function App() {
   return (
     <div>
       <Header/>
+      <Box style={{marginTop:54}}>
+      <Home/>
+      </Box>
     </div>
   );
 }
