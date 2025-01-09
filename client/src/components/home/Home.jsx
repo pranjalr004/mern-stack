95332f993b127ca031ff225ad80fa648e973c2cb
pranjalr004 <pranjalr376@gmail.com>
2025-01-09 13:15:10 +0530

    Redux Implementation and fetched the data using Redux Hooks

--- client/src/components/home/Home.jsx
+++ client/src/components/home/Home.jsx
@@ -1,12 +1,24 @@
 import { Box,styled } from "@mui/material"
 import Banner from "./Banner"
 import Navbar from "./Navbar"
+import { useEffect } from "react"
+import { getProducts } from "../../redux/actions/productActions"
+import { useDispatch } from "react-redux"
+import { useSelector } from "react-redux"
 
 const Component=styled(Box)`
     padding:10px;
     background:#F2F2F2;`
 
 const Home=()=>{
+
+   const {products}= useSelector(state=>state.getProducts)
+   console.log(products)
+    const dispatch=useDispatch()
+
+    useEffect(()=>{
+       dispatch(getProducts())
+    },[dispatch])
     return (
         <>
             <Navbar/>
--- server/routes/route.js
+++ server/routes/route.js
@@ -1,10 +1,11 @@
 import express from "express"
 const router=express.Router()
 import { userSignup,userLogin } from "../controller/user-controller.js"
-
+import { getProducts } from "../controller/product-controller.js"
 router.post("/signup",userSignup)
 
 router.post("/login",userLogin)
 
+router.get("/products",getProducts)
 
 export default router
